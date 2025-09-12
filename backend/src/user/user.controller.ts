d561e5361de5aa6179aacfee24679a6e831c3601
Edmilzon <edmilzon.luna@gmail.com>
2025-09-12 10:08:48 -0600

    fead: login and register finality controller

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserDto } from "./dto/user.dto";
+import { log } from "console";
+
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ){}
+
+    @Post('register')
+    async resgisterUser(@Body() date: UserDto){
+        const newUser = await this.userService.createUser(date);
+        return {
+            message: "Usuario creado correctamente",
+            status: 201,
+            newUser
+        }
+    }
+
+    @Post('login')
+    async loginUser(@Body() date: UserDto){
+        const login = await this.userService.loginUser(date);
+        const { password, ...userNotPassword } = login.user;
+        return {
+            message: "Login exitoso",
+            status: 200,
+            token: login.token,
+            login: userNotPassword
+        }
+    }
+
+}
