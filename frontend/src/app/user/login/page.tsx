5f5cffde51201d8f18f585abae90636f0e48c5be
Shoutt4 <jhonvergara437@gmail.com>
2025-10-02 02:21:21 -0400

    cambios-sv

--- frontend/src/app/user/login/page.tsx
+++ frontend/src/app/user/login/page.tsx
@@ -84,7 +84,7 @@ export default function LoginPage() {
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-black"
+                  className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-black cursor-pointer"
                 >
                   {showPassword ? "no ver" : "ver"}
                 </button>
@@ -108,7 +108,7 @@ export default function LoginPage() {
             
             <button
               type="submit"
-              className="w-full rounded-lg bg-[#306998] px-4 py-2 text-white font-semibold hover:bg-[#205375] transition transform hover:scale-105 shadow-md"
+              className="w-full rounded-lg bg-[#306998] px-4 py-2 text-white font-semibold hover:bg-[#205375] transition transform hover:scale-105 shadow-md cursor-pointer"
             >
               Iniciar sesión
             </button>
@@ -121,19 +121,19 @@ export default function LoginPage() {
           </div>
          
           <div className="flex gap-4">
-            <button className="flex-1 border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-100 transition transform hover:scale-105 shadow-sm">
+            <button className="flex-1 border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-100 transition transform hover:scale-105 shadow-sm cursor-pointer">
               <Image src="/img/google.png" alt="Google" width={20} height={20} />
-              <span className="text-black font-medium">Google</span>
+              <span className="text-black font-medium ">Google</span>
             </button>
-            <button className="flex-1 border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-100 transition transform hover:scale-105 shadow-sm">
+            <button className="flex-1 border rounded-lg py-2 flex items-center justify-center gap-2 hover:bg-gray-100 transition transform hover:scale-105 shadow-sm cursor-pointer">
               <Image src="/img/apple.png" alt="Apple" width={20} height={20} />
-              <span className="text-black font-medium">Apple</span>
+              <span className="text-black font-medium ">Apple</span>
             </button>
           </div>
           
-          <p className="text-center text-sm text-gray-600">
+          <p className="text-center text-sm text-gray-600 ">
             ¿No tienes cuenta?{" "}
-            <Link href="/user/register" className="text-[#FFD43B] font-medium hover:underline">
+            <Link href="/user/register" className="text-[#FFD43B] font-medium hover:underline cursor-pointer">
              Regístrate
           </Link>
           </p>
