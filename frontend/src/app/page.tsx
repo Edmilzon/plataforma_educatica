a0b5292c200ddb43887d45bfb9736599bdc77c64
giuli <giulianaqs818@gmail.com>
2025-10-28 12:20:25 -0400

    fix: error contrasenia

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 // import Image from "next/image";
-
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 const HOME = () => {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 };
